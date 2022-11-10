7253ffd61ab0c98b30ced824bff0b8669f5093cc
ROSTAMI Milad <milad.rostami@artfx34.lan>
2022-11-10 12:41:04 +0100

    add player to scene
    add horizontal and vertical movement controls to the player.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rigid_body;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rigid_body.MovePosition(rigid_body.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
