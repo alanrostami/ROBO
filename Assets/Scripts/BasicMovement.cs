1648291d051df207e0434bb752eee42d622f26ad
alanrostami <alan.rostami@artfx.fr>
2022-11-16 19:47:09 +0100

    [add] add shooting direction to laser prefab

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -13,15 +13,25 @@ public class BasicMovement : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
 
-        if (Input.GetButtonDown("ShootLaser"))
-        {
-            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
-            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(8.0f, 0.0f);
-        }
+        ShootLaserBeams();
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         
         transform.position = transform.position + movement * moveSpeed * Time.deltaTime;
+    }
+
+    private void ShootLaserBeams()
+    {
+        Vector2 shootingDirection = new Vector2(Input.GetAxis("Horizontal"), 0.0f);
+
+        shootingDirection.Normalize();
+
+        if (Input.GetButtonDown("ShootLaser"))
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().velocity = shootingDirection * 8.0f;
+            Destroy(laser, 2.0f);
+        }
     }
 }
