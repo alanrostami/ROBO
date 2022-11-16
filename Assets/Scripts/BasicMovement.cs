ebec7442c1ec48467c92b7fc9311ab7bfe6c63c0
alanrostami <alan.rostami@artfx.fr>
2022-11-16 19:33:16 +0100

    [add] add laser prefab to ROBO

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
     public Animator animator;
+
+    public GameObject laserPrefab;
 
     void Update()
     {
-        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        if (Input.GetButtonDown("ShootLaser"))
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(8.0f, 0.0f);
+        }
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
         
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime;
+        transform.position = transform.position + movement * moveSpeed * Time.deltaTime;
     }
 }
