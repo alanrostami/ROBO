55ea6c5b2d0f9c6e207cdd85ff39cb04b014666c
alanrostami <alan.rostami@artfx.fr>
2022-11-21 18:56:41 +0100

    [add] alien prefabs

---
+++ Assets/Scripts/AlienLogic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienLogic : MonoBehaviour
+{
+    public void SpawnAlien()
+    {
+        Debug.Log("Alien Here!");
+    }
+}
