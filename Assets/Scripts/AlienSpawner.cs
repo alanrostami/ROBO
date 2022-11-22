d315286ed1800b01b79d17492c2d952602988755
alanrostami <alan.rostami@artfx.fr>
2022-11-22 19:05:07 +0100

    [modify] alien spawner

--- Assets/Scripts/AlienSpawner.cs
+++ Assets/Scripts/AlienSpawner.cs
@@ -1,24 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
+
+public class AlienWave
+{
+    public string waveTitle;
+    public int numberOfAliens;
+    public GameObject[] typeOfAliens;
+    public float spawnInterval;
+}
 
 public class AlienSpawner : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField] AlienWave[] alienWaves;
     private GameObject purpleAlienPrefab;
 
-    [SerializeField]
-    private float purpleAlienInterval = 5.0f;
-
-    void Start()
-    {
-        StartCoroutine(spawnAlien(purpleAlienInterval, purpleAlienPrefab));
-    }
-
-    private IEnumerator spawnAlien(float interval, GameObject alien)
-    {
-        yield return new WaitForSeconds(interval);
-        GameObject newAlien = Instantiate(alien, new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-6.0f, 6.0f), 0.0f), Quaternion.identity);
-        StartCoroutine(spawnAlien(interval, alien));
-    }
+    
 }
