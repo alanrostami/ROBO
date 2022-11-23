251638f9053bbece91b6ae0c247bf4713e160c68
alanrostami <alan.rostami@artfx.fr>
2022-11-23 10:08:34 +0100

    [add] spawn waves and types of aliens

--- Assets/Scripts/AlienSpawner.cs
+++ Assets/Scripts/AlienSpawner.cs
@@ -13,8 +13,42 @@ public class AlienWave
 
 public class AlienSpawner : MonoBehaviour
 {
-    [SerializeField] AlienWave[] alienWaves;
-    private GameObject purpleAlienPrefab;
+    public AlienWave[] alienWaves;
+    public Transform[] spawnPoints;
 
-    
+    private AlienWave currentWave;
+    private int currentWaveNumber;
+    private float nextSpawnTime;
+
+    private bool canSpawn = true;
+
+    private void Update()
+    {
+        currentWave = alienWaves[currentWaveNumber];
+        SpawnWave();
+        GameObject[] totalAliens = GameObject.FindGameObjectsWithTag("Alien");
+
+        if (totalAliens.Length == 0 && !canSpawn)
+        {
+            currentWaveNumber++;
+            canSpawn = true;
+        }
+    }
+
+    void SpawnWave()
+    {
+        if (canSpawn && nextSpawnTime < Time.time)
+        {
+            GameObject randomAlien = currentWave.typeOfAliens[Random.Range(0, currentWave.typeOfAliens.Length)];
+            Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(randomAlien, randomPoint.position, Quaternion.identity);
+            currentWave.numberOfAliens--;
+            nextSpawnTime = Time.time + currentWave.spawnInterval;
+
+            if (currentWave.numberOfAliens == 0)
+            {
+                canSpawn = false;
+            }
+        }
+    }    
 }
