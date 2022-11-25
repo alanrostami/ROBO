1a73bbf80bc1eb90fd7fc7073e1086a5a8e732d7
alanrostami <alan.rostami@artfx.fr>
2022-11-25 10:01:36 +0100

    [add] task announce ui

--- Assets/Scripts/TaskInteraction.cs
+++ Assets/Scripts/TaskInteraction.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class TaskInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.tag == "Task")
+        {
+            Debug.Log("Press shift to do the task");
+        }
     }
 }
