95812b460a8e62726df7815a420d64fcaada03be
alanrostami <alan.rostami@artfx.fr>
2022-11-28 11:14:59 +0100

    [add] alien healthbar

---
+++ Assets/Scripts/AlienHealthBar.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlienHealthBar : MonoBehaviour
+{
+    public Slider healthSlider;
+    public Color lowHealth;
+    public Color highHealth;
+    public Vector3 heightOffset;
+
+    public void SetHealth(float health, float maxHealth)
+    {
+        healthSlider.gameObject.SetActive(health < maxHealth);
+        healthSlider.value = health;
+        healthSlider.maxValue = maxHealth;
+
+        healthSlider.fillRect.GetComponentInChildren<Image>().color = Color.Lerp(lowHealth, highHealth, healthSlider.normalizedValue);
+    }
+
+    void Update()
+    {
+        healthSlider.transform.position = Camera.main.WorldToScreenPoint(transform.parent.position + heightOffset);
+    }
+}
---
+++ Assets/Scripts/AlienHitPoints.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienHitPoints : MonoBehaviour
+{
+    public float hitPoints;
+    public float maxHitPoints = 5;
+    public AlienHealthBar greenAlienHealthBar;
+
+    void Start()
+    {
+        hitPoints = maxHitPoints;
+        greenAlienHealthBar.SetHealth(hitPoints, maxHitPoints);
+    }
+
+    public void TakeHit(float damage)
+    {
+        hitPoints -= damage;
+        greenAlienHealthBar.SetHealth(hitPoints, maxHitPoints);
+
+        if (hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
